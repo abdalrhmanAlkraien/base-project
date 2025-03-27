8c5061dec98ee781b3f10dfe2bf5895e3d1995f9
Abd-alrhman Alkraien <abdalrhmanalkraien@gmail.com>
2025-03-27 19:17:32 +0200

    feat: enable swagger.

--- alrajhi-report/src/main/java/com/alrjhi/controller/ReportController.java
+++ alrajhi-report/src/main/java/com/alrjhi/controller/ReportController.java
@@ -5,6 +5,11 @@ import com.alrjhi.dto.request.AuthorRequest;
 import com.alrjhi.dto.response.AuthorResponse;
 import com.alrjhi.service.ReportService;
 import com.alrjhi.util.GenericResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -39,6 +44,25 @@ import java.util.Map;
 public class ReportController {
 
     private final ReportService reportService;
+
+    @Operation(summary = "create a new API to generate report")
+    @ApiResponses(value = {
+
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(
+                            mediaType = "application/pdf",
+                            schema = @Schema(implementation = AuthorRequest.class)
+                    )
+            }, description = "Generate the report"),
+
+            @ApiResponse(responseCode = "400",content = {
+                    @Content
+            }, description = "Bad request"),
+
+            @ApiResponse(responseCode = "404",content = {
+                    @Content
+            }, description = "Not Found")
+    })
     @GetMapping
     public ResponseEntity<ByteArrayResource> getReport() throws URISyntaxException, JRException {
 
